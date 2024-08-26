a213a60968b3e262343268cb8735ca5f07058bd2
super27036 <144255894+super27036@users.noreply.github.com>
2024-08-26 13:24:59 +0800

    Update super-lede-slim.sh

--- scripts/super-lede-slim.sh
+++ scripts/super-lede-slim.sh
@@ -98,9 +98,19 @@ git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 rm -rf ../../customfeeds/luci/applications/luci-app-argon-config
 
 # Add luci-theme
+# git clone https://github.com/DHDAXCW/theme
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+# git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+
+# Add luci-theme
 git clone https://github.com/DHDAXCW/theme
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+
+rm -rf ../../customfeeds/luci/themes/luci-theme-argon
+rm -rf ../../customfeeds/luci/themes/luci-theme-argon-mod
+rm -rf ../../customfeeds/luci/applications/luci-app-argon-config
+
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config
 
 # Add subconverter
 git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
