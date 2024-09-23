bcbd7e272dd67bbc08063dd055eb09bd1ccaea2c
super27036 <144255894+super27036@users.noreply.github.com>
2024-09-23 23:25:24 +0800

    Update DHDAXCW-super-2024.sh

--- scripts/DHDAXCW-super-2024.sh
+++ scripts/DHDAXCW-super-2024.sh
@@ -17,6 +17,10 @@ rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
 rm -rf openwrt-package/verysync
 rm -rf openwrt-package/luci-app-verysync
 
+# Add luci-app-unblockneteasemusic
+rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
+git clone --depth=1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git
+
 # 删除旧的 luci-theme-argon 和 luci-theme-argon-mod 和 luci-app-argon-config
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon-mod
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon
