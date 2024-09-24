1d66d2c726d389de6eb7f12671d0b414ba0ad9e3
super27036 <144255894+super27036@users.noreply.github.com>
2024-09-24 21:05:03 +0800

    Update hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -6,27 +6,27 @@
 pushd customfeeds
 
 # Add luci-app-eqos
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
+# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-eqos luci/applications/luci-app-eqos
 
 # Add luci-proto-modemmanager
-svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
+# svn co https://github.com/immortalwrt/luci/trunk/protocols/luci-proto-modemmanager luci/protocols/luci-proto-modemmanager
 
 # Add luci-app-gowebdav
-svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
-svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
+# svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav luci/applications/luci-app-gowebdav
+# svn co https://github.com/immortalwrt/packages/trunk/net/gowebdav packages/net/gowebdav
 
 # Add tmate
-git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
+# git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 
 # Add gotop
-svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
+# svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
 
 # Add minieap
-svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
+# svn co https://github.com/immortalwrt/packages/trunk/net/minieap packages/net/minieap
 
 # Replace smartdns with the official version
-rm -rf packages/net/smartdns
-svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
+# rm -rf packages/net/smartdns
+# svn co https://github.com/openwrt/packages/trunk/net/smartdns packages/net/smartdns
 popd
 
 # Set to local feeds
