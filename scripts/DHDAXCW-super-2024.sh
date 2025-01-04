040ddccd831c11298af8f83a74402d4190f38cb8
super27036 <144255894+super27036@users.noreply.github.com>
2025-01-04 19:49:24 +0800

    Update DHDAXCW-super-2024.sh

--- scripts/DHDAXCW-super-2024.sh
+++ scripts/DHDAXCW-super-2024.sh
@@ -75,16 +75,16 @@ cd ..
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify default hostname
-sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/SUPERouter/g' package/base-files/files/bin/config_generate
 
 # Password change
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
+# sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRKR0jNadn3YLrSQ0:18690:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 
 # Disable IPv6
-sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+# sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
 
 # Fix uboot problem (DHDAXCW 版本不需要这个操作，lede版本需要）
 # sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
