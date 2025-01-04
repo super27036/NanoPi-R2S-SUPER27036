371b8ce84c475cdccb84922c1e4eef293e21cea3
super27036 <144255894+super27036@users.noreply.github.com>
2025-01-04 20:11:35 +0800

    Update DHDAXCW-super-2024.sh

--- scripts/DHDAXCW-super-2024.sh
+++ scripts/DHDAXCW-super-2024.sh
@@ -74,6 +74,7 @@ cd ..
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
+#---------以下部分改到了modify-default.sh文件里面执行
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
@@ -85,6 +86,7 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Disable IPv6
 # sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+#---------以上部分改到了modify-default.sh文件里面执行
 
 # Fix uboot problem (DHDAXCW 版本不需要这个操作，lede版本需要）
 # sed -i '/^UBOOT_TARGETS := rk3528-evb rk3588-evb/s/^/#/' package/boot/uboot-rk35xx/Makefile
