1fd55483cef4b8e2b06204b14ee63b64f2fc1bba
super27036 <144255894+super27036@users.noreply.github.com>
2025-01-04 23:46:41 +0800

    Update modify-default.sh

--- scripts/modify-default.sh
+++ scripts/modify-default.sh
@@ -12,3 +12,10 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/$1$S2TRFyMU$E8fE0RRK
 
 # Disable IPv6
 sed -i 's/def_bool y/def_bool n/g' config/Config-build.in
+
+# Modify zzz-default-settings
+sed -i '/http/d' package/lean/default-settings/files/zzz-default-settings
+sed -i '/18.06/d' package/lean/default-settings/files/zzz-default-settings
+export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+export date_version=$(date -d "$(rdate -n -4 -p ntp.aliyun.com)" +'%Y-%m-%d')
+sed -i "s/${orig_version}/SUPER-DHDAXCW ${orig_version} (${date_version})/g" zzz-default-settings
