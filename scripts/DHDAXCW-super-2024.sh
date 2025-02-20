b9a8200745fc58bd9f80617c08f5086dd3163b74
super27035 <superssatellite@gmail.com>
2025-02-20 09:21:28 +0800

    Update DHDAXCW-super-2024.sh

--- scripts/DHDAXCW-super-2024.sh
+++ scripts/DHDAXCW-super-2024.sh
@@ -39,7 +39,7 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config 
 
 # Add luci-app-ssr-plus
 # git clone --depth=1 https://github.com/fw876/helloworld
-git clone --depth=1 https://github.com/super27035/helloworld-2
+git clone --depth=1 https://github.com/super27036/helloworld
 
 # Add luci-app-vssr <M>
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
