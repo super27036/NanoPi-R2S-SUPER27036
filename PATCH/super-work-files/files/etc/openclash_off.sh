f398970398c4e7fe91424e703cc4691fe72a1ab1
super27036 <144255894+super27036@users.noreply.github.com>
2025-02-27 15:57:48 +0800

    Delete PATCH directory

--- PATCH/super-work-files/files/etc/openclash_off.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-uci set openclash.config.enable='0'
-uci commit openclash
-/etc/init.d/openclash stop
--- PATCH/super-work-files/files/etc/openclash_on.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-uci set openclash.config.enable='1'
-uci commit openclash
-/etc/init.d/openclash start
